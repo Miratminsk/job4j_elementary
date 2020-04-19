285d4217f8326e35427bd436b72472ff4b19242e
Mirat Rakhmanau <33043979+Miratminsk@users.noreply.github.com>
2020-04-19 12:03:23 -0500

    6.5.2. Поиск минимума в диапазоне. [#179804]

--- src/ru/job4j/array/Min.java
+++ src/ru/job4j/array/Min.java
@@ -5,11 +5,8 @@ public class Min {
     public static int findMin(int[] array) {
         int min = array[0];
         for (int index = 0; index < array.length; index++) {
-            // проверить, что эталон больше, чем элемент. записать в эталон элемент массива.
             if (min > array[index]) {
-                int temp = min;
                 min = array[index];
-                array[index] = temp;
             }
         }
         return min;
