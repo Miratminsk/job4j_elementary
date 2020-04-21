76079fe4babc4b42f6a87bdf65fac6f5852df7fb
Mirat Rakhmanau <33043979+Miratminsk@users.noreply.github.com>
2020-04-21 13:14:35 -0500

    4. Расстояние между точками в трехмерном пространстве. [#122643]

--- test/ru/job4j/condition/PointTest.java
+++ test/ru/job4j/condition/PointTest.java
@@ -17,7 +17,7 @@ public class PointTest {
     }
 
     @Test
-    public void whenPoints0And0And0And0Distance0() {
+    public void whenAllPoints0Distance0() {
         Point point1 = new Point(0, 0);
         Point point2 = new Point(0, 0);
         double expected = 0.00;
@@ -31,6 +31,33 @@ public class PointTest {
         Point point2 = new Point(17, 6);
         double expected = 26.00;
         double out = point1.distance(point2);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void whenPoints17And29And7And88And86And100Distance130And14() {
+        Point point1 = new Point(17, 29, 7);
+        Point point2 = new Point(88, 86, 100);
+        double expected = 130.14;
+        double out = point1.distance3d(point2);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void whenAllPoints0ADistance0() {
+        Point point1 = new Point(0, 0, 0);
+        Point point2 = new Point(0, 0, 0);
+        double expected = 0.00;
+        double out = point1.distance3d(point2);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void whenPointsMinus7AndMinus4AndMinus15And17And6And30Distance51And97() {
+        Point point1 = new Point(-7, -4, -15);
+        Point point2 = new Point(17, 6, 30);
+        double expected = 51.97;
+        double out = point1.distance3d(point2);
         Assert.assertEquals(expected, out, 0.01);
     }
 }
