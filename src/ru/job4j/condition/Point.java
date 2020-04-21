aef3dc547679589116ac9a267ec8c7b8d93c78ed
Mirat Rakhmanau <33043979+Miratminsk@users.noreply.github.com>
2020-04-21 11:00:25 -0500

    2.1. Рефакторинг - Расстояние между точками. [#122587]

--- src/ru/job4j/condition/Point.java
+++ src/ru/job4j/condition/Point.java
@@ -1,19 +1,27 @@
 package ru.job4j.condition;
 
+import static java.lang.Math.sqrt;
+import static java.lang.Math.pow;
+
 public class Point {
 
-    public static double distance(int x1, int y1, int x2, int y2) {
-        return Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
-    }
+        private int x;
 
-    public static void main(String[] args) {
-        double result = Point.distance(0, 0, 2, 0);
-        System.out.println("result (0, 0) to (2, 0) " + result);
+        private int y;
 
-        result = Point.distance(17, 29, 88, 86);
-        System.out.println("result (17, 29) to (88, 86) " + result);
+        public Point(int first, int second) {
+            this.x = first;
+            this.y = second;
+        }
 
-        result = Point.distance(91, 53, 29, 78);
-        System.out.println("result (91, 53) to (29, 78) " + result);
-    }
+        public double distance(Point that) {
+            return sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2));
+        }
+
+        public static void main(String[] args) {
+            Point a = new Point(0, 0);
+            Point b = new Point(0, 2);
+            double dist = a.distance(b);
+            System.out.println(dist);
+        }
 }
--- test/ru/job4j/condition/PointTest.java
+++ test/ru/job4j/condition/PointTest.java
@@ -9,34 +9,28 @@ public class PointTest {
 
     @Test
     public void whenPoints17And29And88And86Distance11() {
-        int x1 = 17;
-        int y1 = 29;
-        int x2 = 88;
-        int y2 = 86;
+        Point point1 = new Point(17, 29);
+        Point point2 = new Point(88, 86);
         double expected = 91.04;
-        double out = Point.distance(x1, y1, x2, y2);
+        double out = point1.distance(point2);
         Assert.assertEquals(expected, out, 0.01);
     }
 
     @Test
     public void whenPoints0And0And0And0Distance0() {
-        int x1 = 0;
-        int y1 = 0;
-        int x2 = 0;
-        int y2 = 0;
+        Point point1 = new Point(0, 0);
+        Point point2 = new Point(0, 0);
         double expected = 0.00;
-        double out = Point.distance(x1, y1, x2, y2);
+        double out = point1.distance(point2);
         Assert.assertEquals(expected, out, 0.01);
     }
 
     @Test
     public void whenPointsMinus7AndMinus4And17And6Distance26() {
-        int x1 = -7;
-        int y1 = -4;
-        int x2 = 17;
-        int y2 = 6;
+        Point point1 = new Point(-7, -4);
+        Point point2 = new Point(17, 6);
         double expected = 26.00;
-        double out = Point.distance(x1, y1, x2, y2);
+        double out = point1.distance(point2);
         Assert.assertEquals(expected, out, 0.01);
     }
 }
